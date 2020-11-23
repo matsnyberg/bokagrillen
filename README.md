5560b3886640dd4018bea6e16413660fa6c7bb42
Mats Nyberg <info@jollobajano.com>
2020-11-23 23:14:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Boka Grillen
+============
+
+
+
+En plugin för brf:s hemsida för att kunna boka grillen.
+
+Plugin för Wordpress.
+
+
---
+++ src/php/bokagrillen.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Boka Grillen
+ * Plugin URI: http://www.mywebsite.com/my-first-plugin
+ * Description: The very first plugin that I have ever created.
+ * Version: 1.0
+ * Author: Mats Nyberg
+ * Author URI: http://www.mywebsite.com
+ */
